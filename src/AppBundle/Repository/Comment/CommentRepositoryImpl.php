e48a964208a893124dbef5e3e57d7bf5cb7c105e
Carlos Arteche <idrigan@gmail.com>
2017-08-24 19:12:21 +0200

    Bug en mostrar los comentarios de los amigos

--- src/AppBundle/Repository/Comment/CommentRepositoryImpl.php
+++ src/AppBundle/Repository/Comment/CommentRepositoryImpl.php
@@ -21,20 +21,49 @@ class CommentRepositoryImpl extends EntityRepository implements CommentRepositor
     public function getAll($idUser)
     {
 
+        $friends = $this->getFriends($idUser);
+        $users = array();
+        foreach ($friends as $f) {
+            $users[] = $f['id_user'];
+        }
+        $users[] = $idUser;
 
-        $sql =" SELECT p.*,u.email 
-              FROM publication p LEFT JOIN user_friend uf ON p.id_user = uf.id_user_friend AND  uf.id_user =:idUser
-              LEFT JOIN user u ON u.id = uf.id_user_friend OR u.id = p.id_user
-              WHERE p.id_user =:idUser AND p.active =1 ORDER BY p.date_create DESC";
+        $result = $this->getComments($users);
+        return $result;
+
+    }
+
+    private function getFriends($idUser){
+        $sql =" SELECT uf.id_user_friend FROM  user_friend uf  WHERE  uf.id_user = :idUser AND accepted = 1";
 
         $query = $this->getEntityManager()->getConnection()->prepare($sql);
         $query->bindValue('idUser', $idUser);
         $query->execute();
-        $result =  $query->fetchAll();
+        $data = $query->fetchAll();
 
 
+        $sql =" SELECT uf.id_user FROM user_friend uf WHERE  uf.id_user_friend = :idUser  AND accepted = 1";
 
-        foreach ($result as $index=>$row){
+        $query = $this->getEntityManager()->getConnection()->prepare($sql);
+        $query->bindValue('idUser', $idUser);
+        $query->execute();
+        return array_merge($query->fetchAll(),$data);
+    }
+
+
+    private function  getComments($users){
+        $users = join(",",$users);
+        $sql = " SELECT p.*,u.email FROM publication p
+                LEFT JOIN user u ON p.id_user = u.id
+                WHERE p.active =1 AND p.id_user IN ($users) ORDER BY p.date_create DESC";
+
+        $query = $this->getEntityManager()->getConnection()->prepare($sql);
+    //    $query->bindValue('idUser', $idUser);
+        $query->execute();
+        $result = $query->fetchAll();
+
+
+        foreach ($result as $index => $row) {
 
             $sql = "SELECT rp.*,u.email FROM response_publication rp
                      LEFT JOIN user u ON  u.id = rp.id_user
@@ -43,13 +72,12 @@ class CommentRepositoryImpl extends EntityRepository implements CommentRepositor
             $query = $this->getEntityManager()->getConnection()->prepare($sql);
             $query->bindValue('idPublication', $row['id']);
             $query->execute();
-            $responses =  $query->fetchAll(\PDO::FETCH_CLASS,ResponsePublication::class);
+            $responses = $query->fetchAll(\PDO::FETCH_CLASS, ResponsePublication::class);
 
 
             $result[$index]['responses'] = $responses;
 
         }
-
 
         return $result;
     }
